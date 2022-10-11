a87032bde5d42501cf592cbc8112ebedde3e5c2d
TaisiiaK <taisiiakompaniets@gmail.com>
2022-10-11 18:12:40 +0300

    add logo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <img
+          src="https://www.shecodes.io/assets/branding/logo-shecodes-f9fa0540d113c086f61eb6e89466c0cbd24a42163b6a96d4b01da078803f53ee.png"
+          alt="logo"
+          className="App-logo img-fluid"
+        />
+      </header>
     </div>
   );
 }
